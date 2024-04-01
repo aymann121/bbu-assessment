496e03e06945f311c1d7d5de39b45dc17a1203ac
IcyBroom <icybroom@gmail.com>
2024-03-31 21:58:08 -0400

    favicon + header

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -8,7 +8,7 @@ export default function Head() {
         <title>BBU Takehome Website</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="Generated with create next app, created by Ayman Noreldaim" />
-        <link rel="icon" href="./favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
 
 import "./globals.css"; // allows the use of tailwinc.css
 import Footer from "./components/footer"; // imports the footer component
+import Header from "./head"; // imports the header component
 
+export const metadata = {
+  title: "Birth By Us Assessment",
+  description: "Created by Ayman Noreldaim",
+  icon: "./favicon.ico",
+};
 
 export default function RootLayout({ children }) {
   /*
@@ -10,6 +16,7 @@ export default function RootLayout({ children }) {
   */
   return (
     <html lang="en">
+      <Header />
       <body className = "min-h-screen bg-gradient-to-b to-[#be4f5e] from-[#f8c1af]">
         <main className = "flex flex-col min-h-screen"> 
           <div className = "grow">{children}</div>
